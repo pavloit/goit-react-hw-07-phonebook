20c3f1accdca56c846bd1f264147420b9e2ebeeb
pavloit <dba.devop@gmail.com>
2024-01-27 22:47:22 +0200

    partial work

--- src/index.js
+++ src/index.js
@@ -1,40 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from 'components/App';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './redux/contactsSlice';
+import App from './components/App';
+import './index.css';
+import { fetchContacts } from './redux/contactsOperations';
 
-// Load from local storage
-const loadState = () => {
-  try {
-    const serializedState = localStorage.getItem('state');
-    if (serializedState === null) {
-      return undefined;
-    }
-    return JSON.parse(serializedState);
-  } catch (err) {
-    return undefined;
-  }
-};
-
-// Load the persisted state from local storage.
-const persistedState = loadState();
-
-// Configure the Redux store with the contacts reducer and the persisted state.
 const store = configureStore({
   reducer: {
     contacts: contactsReducer,
   },
-  preloadedState: persistedState,
 });
 
-store.subscribe(() => {
-  localStorage.setItem('state', JSON.stringify(store.getState()));
-});
+// Load the contacts from the backend when the store is created.
+store.dispatch(fetchContacts());
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
--- src/redux/contactsOperations.js
+++ src/redux/contactsOperations.js
@@ -5,7 +5,8 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 axios.defaults.baseURL = 'https://65b406d0770d43aba47ad797.mockapi.io/contacts';
 
 export const fetchContacts = createAsyncThunk('contacts/fetchAll', async () => {
-    return await axios.get('').data;
+    const response = await axios.get('');
+    return response.data;
 });
 
 export const addContact = createAsyncThunk('contacts/addContact', async (contact) => {
@@ -16,9 +17,3 @@ export const deleteContact = createAsyncThunk('contacts/deleteContact', async (i
     await axios.delete('', id);
     return id;
 });
-
-// {
-//     "id": 1706375073791,
-//         "name": "hello",
-//             "number": "343-44-44"
-// }
